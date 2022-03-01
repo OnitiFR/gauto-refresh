4b7b3d568c948047e14f7f54c18c4fb0c34749a2
Julien Xfennec <xfennec@cqfd-corp.org>
2022-03-01 16:55:31 +0100

    add -c flag to display a conditional script sample

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 type FlagValues []string
 
-const Version = "0.0.3"
+const Version = "0.0.4"
 
 var ListenPort string
 var Files FlagValues
@@ -24,6 +24,7 @@ func (i *FlagValues) String() string {
 func main() {
 	port := flag.Int("p", 8888, "listening port")
 	flag.Var(&Files, "f", "file to watch (mutliple -f accepted, default = current dir)")
+	conditional := flag.Bool("c", false, "display a conditional script sample")
 	version := flag.Bool("v", false, "show version")
 	flag.Parse()
 
@@ -41,7 +42,13 @@ func main() {
 	fmt.Println("Watching", Files)
 	fmt.Println("Add the follow line to your HTML page:")
 	fmt.Println("--")
-	fmt.Printf("<script src=\"http://%s/refresh\"></script>\n", ListenPort)
+	if *conditional {
+		fmt.Println(`<script>
+  if (location.hostname === 'localhost') document.write('<script src="http://localhost:8888/refresh"></' + 'script>')
+</script>`)
+	} else {
+		fmt.Printf("<script src=\"http://%s/refresh\"></script>\n", ListenPort)
+	}
 	fmt.Println("--")
 	StartServer()
 }
